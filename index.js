474c2ada3e4d8d960be9f6fdb9c54c08b9e0d0df
Geoff Phillips <geoffreymartinphillips@gmail.com>
2018-12-02 17:45:34 -0800

    Add oauth_token environment variable

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 const express    = require('express');
 const bodyParser = require('body-parser');
 
-const { randomChar, generateNonce } = require('./lib/oauth-helpers.js');
+const { randomChar, generateNonce, createSignature } = require('./lib/oauth-helpers.js');
 
 const app             = express();
 const PORT            = process.env.PORT || 8080;
-const CONSUMER_KEY    = process.env.TWITTER_CONSUMER_API_KEY;
-
 
 Authorization:
-OAuth oauth_consumer_key="xvz1evFS4wEEPTGEFPHBog",
-oauth_nonce="kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg",
+OAuth oauth_consumer_key=process.env.TWITTER_CONSUMER_API_KEY,
+oauth_nonce=generateNonce();
 oauth_signature="tnnArxj06cWHq44gCs1OSKk%2FjLY%3D",
 oauth_signature_method="HMAC-SHA1",
-oauth_timestamp="1318622958",
-oauth_token="370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb",
+oauth_timestamp=Date.now(),
+oauth_token=process.env.OAUTH_TOKEN,
 oauth_version="1.0"
 
 app.use(bodyParser.urlencoded({ extended: true }));
