685665464411d239d635e39b5fcedf9637d2d0f3
Geoff Phillips <geoffreymartinphillips@gmail.com>
2018-12-02 17:25:22 -0800

    Create oauth-helpers functions

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express    = require('express');
 const bodyParser = require('body-parser');
 
-const app        = express();
-const PORT       = process.env.PORT || 8080;
+const { randomChar, generateNonce } = require('./lib/oauth-helpers.js');
+
+const app             = express();
+const PORT            = process.env.PORT || 8080;
+const CONSUMER_KEY    = process.env.TWITTER_CONSUMER_API_KEY;
+
+
+Authorization:
+OAuth oauth_consumer_key="xvz1evFS4wEEPTGEFPHBog",
+oauth_nonce="kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg",
+oauth_signature="tnnArxj06cWHq44gCs1OSKk%2FjLY%3D",
+oauth_signature_method="HMAC-SHA1",
+oauth_timestamp="1318622958",
+oauth_token="370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb",
+oauth_version="1.0"
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => {
+  if (authorized) {
+    res.send(search)
+  } else {
+    res.send(login);
+  }
+});
 
 app.listen(PORT, () => {
   console.log("App listening on port " + PORT);
